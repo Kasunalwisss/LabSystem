6a77a5d68a6b271cc376fb7604808432bbec03d1
Kasunalwisss <162720778+Kasunalwisss@users.noreply.github.com>
2024-03-25 18:44:57 +0530

    Update AppointmentRepository.java

--- SpringBoot Development_Backend /src/main/java/pai/project/repository/AppointmentRepository.java
+++ SpringBoot Development_Backend /src/main/java/pai/project/repository/AppointmentRepository.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package pai.project.repository;
 
 import java.util.ArrayList;
@@ -10,10 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import pai.project.domain.Appointment;
 import pai.project.domain.Doctor;
 
-/**
- *
- * @author Michal_Grabowski
- */
+
 public interface AppointmentRepository extends CrudRepository<Appointment, Integer> {
     ArrayList<Appointment> findByUserId(long userId);
     
